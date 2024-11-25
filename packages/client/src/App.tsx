509435314a7041f9d34bbea43c4dc13958f91a34
Andrew Monson <andrewmonsondev@gmail.com>
2024-11-25 07:15:57 -0500

    added barebones client ability to display chat messages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useState, ChangeEvent, MouseEvent } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { HandleConversationMessage } from './utils/graphql/mutations.ts'
 import './App.css'
+import { useMutation } from '@apollo/client'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<string[]>([])
+  const [userMessage, setUserMessage] = useState<string>("")
+
+  const updateMessages = (message: string) => {
+    setMessages(prevMessages => [...prevMessages, message])
+  }
+
+  const [chat] = useMutation(HandleConversationMessage, {
+    variables: {message: userMessage},
+    onCompleted: (data) => updateMessages(data.handleConversationMessage.content),
+    onError: (error) => console.log(error)
+  })
+
+  // console.log(data.handleConversationMessage)
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => setUserMessage(event.target.value)
+
+  const handleSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("clicked")
+    updateMessages(userMessage)
+    const chatResult = await chat({
+      variables: {message: userMessage}
+    })
+
+    console.log(chatResult)
+    console.log(messages)
+  }
 
   return (
     <>
@@ -16,18 +45,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <div>
+          {messages.map((message, i) => (
+            <div key={i}>{message}</div>
+          ))}
+        </div>
+        <input value={userMessage} onChange={handleInputChange}></input>
+        <button onClick={handleSubmit}>Chat</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
