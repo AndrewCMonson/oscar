15fc5f12b428e1b6d5d62917ba714b0451a3b3aa
Andrew Monson <andrewmonsondev@gmail.com>
2024-11-25 05:33:14 -0500

    rescaffolded vite app for client. added apolloclient provider

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { createRoot } from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: 'http://localhost:3005/graphql',
+  cache: new InMemoryCache(),
+})
+
+createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-);
+  </ApolloProvider>,
+)
