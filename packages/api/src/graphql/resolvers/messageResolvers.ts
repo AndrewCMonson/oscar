b75365e6bee7e94fe3e8764021ad91e2827849f5
Andrew <andrewmonsondev@gmail.com>
2024-11-26 10:43:29 -0500

    restored messages resolvers and types

---
+++ packages/api/src/graphql/resolvers/messageResolvers.ts
@@ -0,0 +1,48 @@
+import { prismadb } from "@api/src/config/db.js";
+import { Resolvers } from "@api/types/";
+
+export const messageResolvers: Resolvers = {
+  Query: {
+    messages: async () => {
+      const messages = await prismadb.message.findMany();
+      return messages;
+    },
+    message: async (_, { id }) => {
+      const message = await prismadb.message.findUnique({
+        where: {
+          id: id,
+        },
+      });
+      return message;
+    },
+  },
+  Mutation: {
+    createMessage: async (_, { data }) => {
+      const message = await prismadb.message.create({
+        data: {
+          ...data,
+        },
+      });
+      return message;
+    },
+    updateMessage: async (_, { id, data }) => {
+      const message = await prismadb.message.update({
+        where: {
+          id: id,
+        },
+        data: {
+          ...data,
+        },
+      });
+      return message;
+    },
+    deleteMessage: async (_, { id }) => {
+      const message = await prismadb.message.delete({
+        where: {
+          id: id,
+        },
+      });
+      return message;
+    },
+  },
+};
