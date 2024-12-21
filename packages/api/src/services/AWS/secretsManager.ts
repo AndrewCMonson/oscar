5f02ee5ba46541ebb591a993c79a9afe9f926ccf
Andrew Monson <andrewmonsondev@gmail.com>
2024-12-21 14:48:21 -0500

    added docs to db, secrets manager and escapePW files

--- packages/api/src/services/AWS/secretsManager.ts
+++ packages/api/src/services/AWS/secretsManager.ts
@@ -1,6 +1,19 @@
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
 import { AWSSecrets } from '@api/types/types.js';
 
+/**
+ * Retrieves AWS secrets from AWS Secrets Manager.
+ *
+ * @returns {Promise<AWSSecrets>} A promise that resolves to the AWS secrets.
+ *
+ * @throws Will throw an error if the secret retrieval fails.
+ *
+ * @example
+ * ```typescript
+ * const secrets = await AWSSecretsRetrieval();
+ * console.log(secrets);
+ * ```
+ */
 export const AWSSecretsRetrieval = async () => {
   const SecretId = process.env.AWS_SECRET_ID;
 
