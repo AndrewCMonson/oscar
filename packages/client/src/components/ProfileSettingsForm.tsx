64c4cb236fad2c2c8f03cb6d1297bf51f9722e75
Andrew Monson <andrewmonsondev@gmail.com>
2024-12-26 20:53:08 -0500

    add edit button and styling to profile settings component

--- packages/client/src/components/ProfileSettingsForm.tsx
+++ packages/client/src/components/ProfileSettingsForm.tsx
@@ -1,6 +1,3 @@
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button.tsx";
 import {
   Form,
@@ -19,11 +16,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { User } from "@auth0/auth0-react";
 import { UserMetadata } from "@/hooks/useUserMetadata.tsx";
-import { ResponseStyle, Tone } from '@/types.js';
-
-
+import { ResponseStyle, Tone } from "@/types.js";
+import { User } from "@auth0/auth0-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 const formSchema = z.object({
   username: z.string().min(3),
@@ -37,22 +35,26 @@ const formSchema = z.object({
 
 interface ProfileSettingsFormProps {
   user: User | undefined;
-  userMetadata: UserMetadata;
+  userMetadata: UserMetadata | null;
+  formEditable: boolean;
+  setFormEditable: (editable: boolean) => void;
 }
 
 export const ProfileSettingsForm = ({
   user,
   userMetadata,
+  formEditable,
+  setFormEditable,
 }: ProfileSettingsFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: user?.nickname,
-      chatModel: userMetadata.chatModel,
-      integrations: userMetadata.integrations,
-      preferredLanguage: userMetadata.preferredLanguage,
-      responseStyle: userMetadata.responseStyle,
-      timezone: userMetadata.timezone,
+      username: user?.nickname || "",
+      chatModel: userMetadata?.chatModel || "",
+      integrations: userMetadata?.integrations || [],
+      preferredLanguage: userMetadata?.preferredLanguage || "",
+      responseStyle: userMetadata?.responseStyle || ResponseStyle.Conversational,
+      timezone: userMetadata?.timezone || "",
     },
   });
 
@@ -63,12 +65,15 @@ export const ProfileSettingsForm = ({
           control={form.control}
           name={"username"}
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="text-start lg:w-1/2 mt-2">
               <FormLabel htmlFor="username">Username</FormLabel>
               <FormControl>
-                <Input {...field} id="username" />
+                {formEditable ? (
+                  <Input {...field} id="username" />
+                ) : (
+                  <Input disabled {...field} id="username" />
+                )}
               </FormControl>
-              <FormDescription>A unique name for your project</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -77,13 +82,30 @@ export const ProfileSettingsForm = ({
           control={form.control}
           name={"chatModel"}
           render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="chatModel">Chat Model</FormLabel>
+            <FormItem className="text-start lg:w-1/2 mt-2">
+              <FormLabel htmlFor="chatModel" className="">
+                Chat Model
+              </FormLabel>
               <FormControl>
-                <Input {...field} id="chatModel" />
+                {formEditable ? (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger>
+                      <SelectValue>{field.value}</SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value={"GPT-3.5-turbo"}>
+                        GPT-3.5 Turbo
+                      </SelectItem>
+                      <SelectItem value={"GPT-4o"}>GPT-4o</SelectItem>
+                      <SelectItem value={"GPT-4o-mini"}>GPT-4o Mini</SelectItem>
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <Input disabled {...field} id="chatModel" />
+                )}
               </FormControl>
               <FormDescription>
-                A brief description of your project
+                Your preferred chat model for generating responses
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -93,37 +115,74 @@ export const ProfileSettingsForm = ({
           control={form.control}
           name={"timezone"}
           render={({ field }) => (
-            <FormItem>
-              <FormLabel htmlFor="timezone">Type</FormLabel>
+            <FormItem className="text-start lg:w-1/2 mt-2">
+              <FormLabel htmlFor="timezone">Timezone</FormLabel>
               <FormControl>
-                <Select onValueChange={field.onChange} value={field.value}>
-                  <SelectTrigger>
-                    <SelectValue>{field.value}</SelectValue>
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value={"America/New_York"}>
-                      America/New York
-                    </SelectItem>
-                    <SelectItem value={"America/Chicago"}>
-                      America/Chicago
-                    </SelectItem>
-                    <SelectItem value={"America/Los_Angeles"}>
-                      America/Los Angeles
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
+                {formEditable ? (
+                  <Select onValueChange={field.onChange} value={field.value}>
+                    <SelectTrigger>
+                      <SelectValue>{field.value}</SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value={"America/New_York"}>
+                        America/New York
+                      </SelectItem>
+                      <SelectItem value={"America/Chicago"}>
+                        America/Chicago
+                      </SelectItem>
+                      <SelectItem value={"America/Los_Angeles"}>
+                        America/Los Angeles
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                ) : (
+                  <Input disabled {...field} id="timezone" />
+                )}
               </FormControl>
-              <FormDescription>The type of project</FormDescription>
+              <FormDescription>Your current timezone</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button
-          type="submit"
-          className="w-full sm:w-auto bg-gradient-to-r from-blue-600 to-purple-700 hover:from-blue-700 hover:to-purple-800 text-white shadow-xl hover:scale-105 transition-transform rounded-lg px-6 py-2 mt-4"
-        >
-          {"Save Changes"}
-        </Button>
+        <FormField
+          control={form.control}
+          name={"integrations"}
+          render={({ field }) => (
+            <FormItem className="text-start lg:w-1/2 mt-2">
+              <FormLabel htmlFor="integrations">Integrations</FormLabel>
+              <FormControl>
+                {userMetadata?.integrations?.length > 0 ? userMetadata?.integrations.map((integration, index) => (
+                  <>
+                  <div>{integration}</div>
+                  <Input
+                    key={index}
+                    disabled={!formEditable}
+                    {...field}
+                    id={`integrations.${index}`}
+                  />
+                  </>
+                )):
+                <Input disabled {...field} id="integrations" />}
+              </FormControl>
+              <FormDescription>Your current timezone</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex gap-2">
+          <Button
+            className="w-full sm:w-auto bg-gradient-to-r from-blue-600 to-purple-700 hover:from-blue-700 hover:to-purple-800 text-white shadow-xl hover:scale-105 transition-transform rounded-lg px-6 py-2 mt-4"
+            onClick={() => setFormEditable(!formEditable)}
+          >
+            {formEditable ? "Cancel" : "Edit Profile"}
+          </Button>
+          <Button
+            type="submit"
+            className="w-full sm:w-auto bg-gradient-to-r from-blue-600 to-purple-700 hover:from-blue-700 hover:to-purple-800 text-white shadow-xl hover:scale-105 transition-transform rounded-lg px-6 py-2 mt-4"
+          >
+            {"Save Changes"}
+          </Button>
+        </div>
       </form>
     </Form>
   );
