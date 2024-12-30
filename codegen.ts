57806169e73373e787c21974e86422142de87642
Andrew Monson <andrewmonsondev@gmail.com>
2024-12-29 21:42:02 -0500

    update package.json for universal codegen

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,6 @@ const config: CodegenConfig = {
   schema: "http://localhost:3005/graphql",
   documents: ["./packages/client/src/**/*.{ts,tsx}"],
   overwrite: true,
-  // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   generates: {
     "./packages/types/src/__generatedTypes__/": {
       preset: "client",
