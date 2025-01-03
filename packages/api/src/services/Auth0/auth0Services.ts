9c7c7a0181ef9b26c6b9cc33576af01f7e9fbbf0
Andrew Monson <andrewmonsondev@gmail.com>
2025-01-02 20:31:44 -0500

    refactor githubServices to eliminate redundant functions and explicitly type axios calls

--- packages/api/src/services/Auth0/auth0Services.ts
+++ packages/api/src/services/Auth0/auth0Services.ts
@@ -1,13 +1,16 @@
-import axios from 'axios';
+import { User } from "auth0";
+import axios from "axios";
+import { Auth0UserWithAccessToken } from "@api/types/types.js";
 
-export const getAuth0User = async (userId: string) => {
-  const domain = process.env.AUTH0_DOMAIN; // e.g., 'your-tenant.auth0.com'
-  const token = process.env.AUTH0_MANAGEMENT_API_TOKEN; // Replace with your generated token
-
+export const getAuth0User = async (
+  userId: string,
+): Promise<Auth0UserWithAccessToken> => {
+  const domain = process.env.AUTH0_DOMAIN;
+  const token = process.env.AUTH0_MANAGEMENT_API_TOKEN;
   const trimmedToken = token?.trim();
 
   try {
-    const response = await axios.get(
+    const { data: userData } = await axios.get<User>(
       `https://${domain}/api/v2/users/${userId}`,
       {
         headers: {
@@ -17,20 +20,36 @@ export const getAuth0User = async (userId: string) => {
       },
     );
 
-    console.log(response.data);
+    if (!userData) {
+      throw new Error("No Auth0 user associated with user ID");
+    }
 
-    return response.data; // User details
+    const access_token = userData.identities?.find(
+      (identity) => identity.provider === "github",
+    )?.access_token;
+
+    if (!access_token) {
+      throw new Error("No github access token associated with user");
+    }
+
+    return {
+      ...userData,
+      access_token,
+    };
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error(
         "Error fetching user:",
         error.response?.data || error.message,
       );
+      throw new Error("Error getting user's Auth0 user");
     } else {
       if (error instanceof Error) {
         console.error("Error fetching user:", error.message);
+        throw new Error("Error getting user's Auth0 user");
       } else {
         console.error("Error fetching user:", error);
+        throw new Error("Error getting user's Auth0 user");
       }
     }
   }
--- packages/api/src/services/Github/githubService.ts
+++ packages/api/src/services/Github/githubService.ts
@@ -1,70 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
+import { getAuth0User } from "@api/src/services/Auth0/auth0Services";
 import dotenv from "dotenv";
 import { Octokit } from "octokit";
-import { getAuth0User } from "@api/src/services/Auth0/auth0Services";
-import { Auth0Identity } from "@api/types/types.js";
-// import { Endpoints } from "@octokit/types";
 
 dotenv.config();
 
 // type CreateIssueParams = Endpoints["POST /repos/{owner}/{repo}/issues"]["parameters"];
-
-export const getUserGithubAccessToken = async (
-  userId: string,
-): Promise<string> => {
-  const domain = process.env.AUTH0_DOMAIN; // e.g., 'your-tenant.auth0.com'
-  const token = process.env.AUTH0_MANAGEMENT_API_TOKEN; // Replace with your generated token
-
-  const trimmedToken = token?.trim();
-
-  try {
-    const response = await axios.get(
-      `https://${domain}/api/v2/users/${userId}`,
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${trimmedToken}`,
-        },
-      },
-    );
-
-    const identities = response.data.identities as Auth0Identity[];
-
-    const access_token = identities.find(
-      (identity) => identity.provider === "github",
-    )?.access_token;
-
-    if (!access_token) {
-      throw new Error("User does not have a Github access token");
-    }
-
-    return access_token;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error(
-        "Error fetching user:",
-        error.response?.data || error.message,
-      );
-    }
-    if (error instanceof Error) {
-      console.error("Error fetching user:", error.message);
-    }
-    throw new Error("Error getting user's Github access token");
-  }
-};
 
 export const createNewRepository = async (
   userId: string,
   repositoryName: string,
 ) => {
   try {
-    const user = await getAuth0User(userId);
-
-    const identities = user.identities as Auth0Identity[];
-
-    const access_token = identities.find(
-      (identity) => identity.provider === "github",
-    )?.access_token;
+    const { access_token } = await getAuth0User(userId);
 
     const octokit = new Octokit({
       auth: access_token,
@@ -88,22 +36,14 @@ export const createNewIssue = async (
   issueBody: string,
 ) => {
   try {
-    const user = await getAuth0User(userId);
-
-    const identities = user.identities as Auth0Identity[];
-
-    const access_token = identities.find(
-      (identity) => identity.provider === "github",
-    )?.access_token;
-
-    console.log(access_token);
+    const { nickname, access_token } = await getAuth0User(userId);
 
     const octokit = new Octokit({
       auth: access_token,
     });
 
     await octokit.request("POST /repos/{owner}/{repo}/issues", {
-      owner: user.nickname,
+      owner: nickname,
       repo: repositoryName,
       title: issueTitle,
       body: issueBody,
